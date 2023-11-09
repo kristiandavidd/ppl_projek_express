b39e5cb73e9d0cd2b6a3773570544617d37f5933
Yusuf <yusufjana18@gmail.com>
2023-11-09 21:04:47 +0700

    feat:add files

---
+++ app/routes/pkl.proutes.js
@@ -0,0 +1,52 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/pkl.controller");
+
+module.exports = function (app){
+    app.use(function(req,res,next){
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
+    app.post(
+        "/pkl",
+        [authJwt.verifyToken, authJwt.getMahasiswaId, authJwt.isMahasiswa],
+        controller.submitPKL
+    );
+
+    app.get(
+        "/pkl",
+        [authJwt.verifyToken, authJwt.getMahasiswaId, authJwt.isMahasiswa],
+        controller.getPKL
+        );
+    
+    app.get(
+        "/all-pkl",
+        [authJwt.verifyToken, authJwt.isDepartemen],
+        controller.getRekapPKL
+    );
+
+    app.get(
+        "/rekap/pkl",
+        [authJwt.verifyToken]
+    );
+    app.get(
+        "/verifikasi/pkl",
+        [authJwt.verifyToken, authJwt.isDosen],
+        controller.getBelumPKL
+    );
+    
+    app.post(
+        "/verifikasi/pkl/:nim",
+        [authJwt.verifyToken, authJwt.isDosen],
+        controller.VerifPKL
+    );
+    
+    app.get(
+        "/pkl/:nim",
+        [authJwt.verifyToken, authJwt.getMahasiswaIdFromNim, authJwt.isKodeWali],
+        controller.downloadPKL
+    );
+}
